1ba25f20221e34e13797373b96c879e93ee28383
chienbui1703 <80864230+chienbui1703@users.noreply.github.com>
2022-09-28 21:55:47 +0700

    push

---
+++ lib/utils/global_variable.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/screens/add_post.dart';
+import 'package:instagram_flutter/screens/newfeed.dart';
+import 'package:instagram_flutter/screens/profile.dart';
+import 'package:instagram_flutter/screens/search.dart';
+
+const webScreenSize = 600;
+
+List<Widget> homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notifications'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
+];
